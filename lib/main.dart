bac429356716c50a81679a723777ff00e50032d1
Rama Tri Agung <ramatriagung91@gmail.com>
2021-05-29 22:16:20 +0700

    custom font

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
+        fontFamily: 'NotoSansJP',
       ),
       home: SplashScreen(),
     );
